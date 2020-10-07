a4e345be872eb4e4d22a45915783618614bff196
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-07 11:59:42 +0300

    Add files via upload

---
+++ create.php
@@ -0,0 +1,15 @@
+<?php
+use yii\widgets\ActiveForm ;
+use frontend\models\Avatar ;
+use yii\helpers\Html ;
+?>
+
+<?php $form = ActiveForm::begin([
+    'options' => ['enctype' => 'multipart/form-data'] 
+ ]) ; ?>
+
+    <?= $form->field($avatar, 'image[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
+
+    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+
+<?php ActiveForm::end() ?>
