330b17662b17b5d7218e674c47fdeacb2cd365e0
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-07 11:58:59 +0300

    The Avatar controller
    Handling the create & upload.

--- AvatarController.php
+++ AvatarController.php
@@ -7,51 +7,6 @@ use yii\web\UploadedFile ;
 
 class AvatarController extends \yii\web\Controller
 {
-   
-    public function actionIndex()
-    {
-        return $this->render('index') ;
-    }
-    
-    public function actionCreate()
-    {
-        $avatar = new Avatar() ;
-      
-        if ($avatar->load(Yii::$app->request->post())) {
-            
-            $image = UploadedFile::getInstance($avatar, 'image') ;
-            $image->saveAs('@app/views/avatar/uploads/'.$image->baseName . '.' . $image->extension) ;
-            $avatar->image = $image->baseName . '.' . $image->extension ;
-            
-            if ($avatar->save()) {
-                return $this->redirect('site/index') ;
-            }
-        }
-        
-            return $this->render('create', [
-                'avatar' => $avatar,
-            ]);
-    }
-    
-} // End of the controller class
-
-
-
-<?php
-
-namespace frontend\controllers;
-use frontend\models\Avatar ;
-use Yii ;
-use yii\web\UploadedFile ;
-
-class AvatarController extends \yii\web\Controller
-{
-   
-    public function actionIndex()
-    {
-        return $this->render('index') ;
-    }
-    
     public function actionCreate()
     {
         $avatar = new Avatar() ;
@@ -70,7 +25,6 @@ class AvatarController extends \yii\web\Controller
                 return $this->redirect('site/index') ;
             }
         }
-        
             return $this->render('create', [
                 'avatar' => $avatar,
             ]);
